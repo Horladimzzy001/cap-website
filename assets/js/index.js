3e99075a2caf838a845d82d396d7f0eb8eb54eeb
OkeleyeVickkk <justusofnigeria@gmail.com>
2023-10-31 10:38:54 +0100

    made the hero section of the home page

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+const observer = new ResizeObserver(() => {});
+
+const words = ["Quality", "Class", "caliber", "Style", "value"];
+
+const textContainer = document.querySelector(".v-main-title .v-clr");
+let wordIndex = 0;
+const removeAndReplace = () => {
+	const div = document.createElement("div");
+	div.classList.add("v-text");
+	div.innerHTML = `${words[wordIndex]},`;
+	textContainer.appendChild(div);
+	wordIndex = (wordIndex + 1) % words.length;
+
+	setTimeout(() => {
+		div.remove();
+	}, 2000);
+};
+removeAndReplace();
+
+setInterval(removeAndReplace, 2000);
