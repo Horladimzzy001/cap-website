d250a33cb030d2019e3cc7e0b103c350dd64c2cb
OkeleyeVickkk <justusofnigeria@gmail.com>
2023-11-19 09:10:32 +0100

    final update

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,6 +53,40 @@ const textContainer = document.querySelector(".v-main-title .v-clr");
 const productTitle = document.querySelector(".v-product-content .v-product-title");
 const aboutProduct = document.querySelector(".v-product-content .v-text");
 const productSubtitle = document.querySelector(".v-product-content .v-subtitle");
+const copyButton = document.querySelector(".v-acct-details .v-copy");
+const accountNumber = document.querySelector(".v-acct-details .v-acct-num");
+const sendReceipt = document.querySelector(".v-send-receipt");
+
+const svg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+<path
+	fill="currentColor"
+	d="M14 8H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V10c0-1.103-.897-2-2-2z" />
+<path fill="currentColor" d="M20 2H10a2 2 0 0 0-2 2v2h8a2 2 0 0 1 2 2v8h2a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
+</svg>
+`;
+
+const success = `
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0h24ZM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018Zm.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="currentColor" d="M19.495 3.133a1 1 0 0 1 1.352.308l.99 1.51a1 1 0 0 1-.155 1.28l-.003.004l-.014.013l-.057.053l-.225.215a83.86 83.86 0 0 0-3.62 3.736c-2.197 2.416-4.806 5.578-6.562 8.646c-.49.856-1.687 1.04-2.397.301l-6.485-6.738a1 1 0 0 1 .051-1.436l1.96-1.768A1 1 0 0 1 5.6 9.2l3.309 2.481c5.168-5.097 8.1-7.053 10.586-8.548Z"/></g></svg>
+`;
+
+if (copyButton) {
+	copyButton.addEventListener("click", async function () {
+		if ("clipboard" in navigator) {
+			const textToCopy = accountNumber.textContent.trim();
+			navigator.clipboard.writeText(textToCopy);
+			const copiedText = await navigator.clipboard.readText();
+			if (copiedText) {
+				copyButton.innerHTML = success;
+				copyButton.disabled = true;
+				setTimeout(() => {
+					copyButton.innerHTML = svg;
+					copyButton.disabled = false;
+				}, 2000);
+			}
+		}
+	});
+}
 
 if (window.location.pathname.includes("product.html")) {
 	if (!check || !params.has("ext")) {
@@ -92,4 +126,15 @@ if (textContainer) {
 	};
 	removeAndReplace();
 	setInterval(removeAndReplace, 2000);
+}
+function sendMessage() {
+	const phoneNumber = "2348057704001";
+	const message = "Hello, I've just made a payment for my recent purchase/order. Could you kindly confirm receipt on your end? Thank you";
+	const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+	window.open(url, "_blank");
+}
+
+if (sendReceipt) {
+	sendReceipt.addEventListener("click", sendMessage);
 }
